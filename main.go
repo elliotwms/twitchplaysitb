18f74d285f7a5bb4899b14af71998a5599e47ff6
Elliot Williams <elliot.sel@gmail.com>
2018-03-10 17:13:10 +0000

    ping pong

--- main.go
+++ main.go
@@ -6,23 +6,72 @@ import (
 	"github.com/go-vgo/robotgo"
 	"os"
 	"time"
+	"errors"
 )
 
 const xOffset = 0
 const yOffset = 45
 
 func main() {
+	//setUpGame()
+
+	username, token, channel, err := getCredentials()
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	c := twitch.NewClient(username, token)
+
+	c.OnNewMessage(handleMessage(c))
+
+	c.Join(channel)
+
+	err = c.Connect()
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func handleMessage(c *twitch.Client) func(channel string, user twitch.User, message twitch.Message) {
+	return func(channel string, user twitch.User, message twitch.Message) {
+		fmt.Printf("%s: %s", user.Username, message.Text)
+		c.Say(channel, message.Text)
+	}
+}
+
+func getCredentials() (username string, token string, channel string, err error) {
+	username, found := os.LookupEnv("TWITCH_USERNAME")
+
+	if !found {
+		return username, token, channel, errors.New("missing username")
+	}
+
+	token, found = os.LookupEnv("TWITCH_TOKEN")
+
+	if !found {
+		return username, token, channel, errors.New("missing token")
+	}
+
+	channel, found = os.LookupEnv("TWITCH_CHANNEL")
+
+	if !found {
+		return username, token, channel, errors.New("missing channel")
+	}
+
+	return username, token, channel, nil
+}
+
+func setUpGame() {
 	ok := robotgo.ShowAlert("Into The Twitch", "Move your app to the top left of the screen and set the resolution to \"Default Windowed\"")
 
 	if ok == 1 {
 		os.Exit(1)
 	}
 
-	setUpGame()
-	play()
-}
-
-func setUpGame() {
 	robotgo.ActiveName("IntoTheBreach")
 
 	bmp := robotgo.CaptureScreen(xOffset, yOffset, 1280, 720)
@@ -37,25 +86,4 @@ func setUpGame() {
 
 	// New Game
 	robotgo.Click()
-}
-
-func play() {
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("TOKEN")
-	channel := os.Getenv("CHANNEL")
-
-	client := twitch.NewClient(username, password)
-
-	client.OnNewMessage(func(channel string, user twitch.User, message twitch.Message) {
-		fmt.Println(message.Text)
-	})
-
-	err := client.Connect()
-
-	if err != nil {
-		panic(err)
-	}
-
-	client.Join(channel)
-	client.Say(channel, "Hello World")
 }
